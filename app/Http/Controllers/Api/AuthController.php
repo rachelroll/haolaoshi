d768eb204a06fc36014febeec5cd4ad3903287f3
lixiuye <hai30@163.com>
2020-04-30 18:24:20 +0800

    complete login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,25 +4,23 @@ namespace App\Http\Controllers\Api;
 
 use App\Questions;
 use App\User;
+use EasyWeChat\Factory;
 use Illuminate\Http\Request;
 
 class AuthController extends BaseController
 {
-
     public function login()
     {
         $code = request()->get('code');
-
         $config = config('wechat.mini_program.default');
         $app = Factory::miniProgram($config);
         $session = $app->auth->session($code);
+        $openid = $session['openid'];
+        //$openid = "oFYRe5Q9G-nWbU30V9T_V19xV2YQ";
+        $user = User::firstOrCreate(['openid' => $openid]);
 
-        $oepnid = $session['oepnid'];
-
-        $user = User::firstOrCreate(['openid',$oepnid]);
         $user->login_time = now();
 
-        // Creating a token without scopes...
         $token = $user->createToken('Token Name')->accessToken;
 
         return $this->success([
