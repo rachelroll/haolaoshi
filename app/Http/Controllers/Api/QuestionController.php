bdf3a40026567fa714a2d6fd6a815d40bded24cd
rotor1996 <hai30@163.com>
2020-06-16 17:06:03 +0800

    fix 支付

--- app/Http/Controllers/Api/QuestionController.php
+++ app/Http/Controllers/Api/QuestionController.php
@@ -224,8 +224,8 @@ class QuestionController extends BaseController
             $wxpay = new WeChatController();
             $result = $wxpay->unifiedOrder($res->id, 2000, $user->openid);
 
-            $timeStamp = now();
-            $pay_sign = MD5('appId='.config('wechat.payment.default.app_id').'&nonceStr='. $result['nonce_str'].'&package=prepay_id='. $result['prepay_id'].'&signType=MD5&timeStamp='.$timeStamp.'&key='. config('wechat.payment.default.key'));
+            $timeStamp = '"'.time().'"';
+            $pay_sign = strtoupper(MD5('appId='.config('wechat.payment.default.app_id').'&nonceStr='. $result['nonce_str'].'&package=prepay_id='. $result['prepay_id'].'&signType=MD5&timeStamp='.$timeStamp.'&key='. config('wechat.payment.default.key')));
 
             return $this->success(compact('result', 'timeStamp', 'pay_sign'));
             //if ($result->result_code == 'SUCCESS') {
