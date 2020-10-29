b758b9dfe7b2135fa929851fd59cf0e58d4eb670
rotor1996 <hai30@163.com>
2020-10-29 22:44:09 +0800

    wechat pay

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 use EasyWeChat\Factory;
 use Illuminate\Support\Facades\Log;
+use Symfony\Component\Console\Question\Question;
 
 class WechatController extends BaseController
 {
@@ -31,15 +32,32 @@ class WechatController extends BaseController
 
         $app = Factory::payment($config);
 
-
         $response = $app->handlePaidNotify(function ($message, $fail) {
-            // 你的逻辑
-            //return true;
-
             Log::info($message);
 
-            // 或者错误消息
-            //$fail('Order not exists.');
+            $order = Question::where('id', $message['out_trade_no'])->get();
+
+            if (!$order || $order->status == 1) { // 如果订单不存在 或者 订单已经支付过了
+                return true; // 告诉微信，我已经处理完了，订单没找到，别再通知我了
+            }
+
+            if ($message['return_code'] === 'SUCCESS') { // return_code 表示通信状态，不代表支付状态
+                // 用户是否支付成功
+                if (array_get($message, 'result_code') === 'SUCCESS') {
+                    $order->updated_at = time(); // 更新支付时间为当前时间
+                    $order->status = 1;
+
+                    // 用户支付失败
+                } elseif (array_get($message, 'result_code') === 'FAIL') {
+                    $order->status = 2;
+                }
+            } else {
+                return $fail('通信失败，请稍后再通知我');
+            }
+
+            $order->save(); // 保存订单
+
+            return true;
         });
 
         return $response;
